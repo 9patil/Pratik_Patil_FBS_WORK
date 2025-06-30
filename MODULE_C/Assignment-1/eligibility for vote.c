1d8ec7569dc9b7ab851fdac7d4907389806491b2
Pratik P. Patil <pratikpatil202003@gmail.com>
2025-06-30 23:40:09 +0530

    Add files via upload

---
+++ MODULE_C/Assignment-1/eligibility for vote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ void main(){
+	
+	int year=17;
+	
+	if (year >= 18){
+	printf("you are eligible for vote");
+     }  
+     
+    else {
+    	printf("you are not eligible for vote");
+	}
+}
