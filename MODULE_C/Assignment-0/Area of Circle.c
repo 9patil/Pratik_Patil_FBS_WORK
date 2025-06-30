062474981722e2bc876d569a60a68febf1e55569
Pratik P. Patil <pratikpatil202003@gmail.com>
2025-07-01 00:47:51 +0530

    Add files via upload

---
+++ MODULE_C/Assignment-0/Area of Circle.c
@@ -0,0 +1,14 @@
+// Area of circle
+
+#include<stdio.h>
+void main() {
+
+  int redious = 6 ;
+  
+  float pie = 3.14;
+  
+  float area = redious * redious * pie ;
+  
+  printf("Area of circle is - %f ", area);
+  
+}
