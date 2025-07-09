93a950403ac61f4e53b46314c89c3a6cdc830d34
Pratik P. Patil <pratikpatil202003@gmail.com>
2025-07-09 10:46:50 +0530

    Add files via upload

---
+++ MODULE_C/TestOnC/Test-2/Test1Q1.c
@@ -0,0 +1,10 @@
+//convert time entered in hh, min and sec into total seconds
+#include<stdio.h>
+void main()
+{
+	int hours=2;
+	int min= hours*60;
+	int sec= hours*min;
+	printf ("%d min & %d sec",min,sec);
+	
+}
---
+++ MODULE_C/TestOnC/Test-2/Test1Q3.c
@@ -0,0 +1,9 @@
+//Print last 2 digit 
+#include<stdio.h>
+void main()
+{
+	int num = 223410;
+    int dig = num%100;
+    
+    printf("%d",dig);
+}
