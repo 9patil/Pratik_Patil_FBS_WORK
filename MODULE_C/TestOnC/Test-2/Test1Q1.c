2f1cdd3f960226b7728e1df0fbfdeded952e87df
Pratik P. Patil <pratikpatil202003@gmail.com>
2025-07-10 16:01:16 +0530

    Update Test1Q1.c

--- MODULE_C/TestOnC/Test-2/Test1Q1.c
+++ MODULE_C/TestOnC/Test-2/Test1Q1.c
@@ -1,10 +1,26 @@
 //convert time entered in hh, min and sec into total seconds
+
 #include<stdio.h>
 void main()
 {
-	int hours=2;
-	int min= hours*60;
-	int sec= hours*min;
-	printf ("%d min & %d sec",min,sec);
+ int hours, minutes, seconds;
+    long int total_seconds; 
+
+    // Prompt for input
+    printf("Enter hours: ");
+    scanf("%d", &hours);
+
+    printf("Enter minutes: ");
+    scanf("%d", &minutes);
+
+    printf("Enter seconds: ");
+    scanf("%d", &seconds);
+	
+	total_seconds = (long int)hours * 3600 + (long int)minutes * 60 + seconds;
+
+    // Print the result
+    printf("Total seconds: %ld\n", total_seconds);
+
+  
 	
 }
